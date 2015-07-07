d13e815bf9a4faf6d6e3977849a72c30c17420d9
Joan <joan.martinez.ripoll@gmail.com>
2015-07-07 10:14:53 +0200

    added routify site methods

--- lib/generators/foxinator/setup/templates/cms/app/controllers/concerns/routify.rb
+++ lib/generators/foxinator/setup/templates/cms/app/controllers/concerns/routify.rb
@@ -1,31 +1,39 @@
 module Routify
   extend ActiveSupport::Concern
 
- included do
-   helper_method :current_namespace, :current_parent
-   
-   def namespaces
-     self.class.namespaces
-   end
- end
+  included do
+    helper_method :current_namespace, :current_parent, :site_url, :site
 
- private
- 
- def current_parent
-   parent if respond_to?(:parent, true)
- end
- 
- def current_namespace
-   namespaces.first
- end
-  
- module ClassMethods
-   def current_namespace
-     namespaces.first
-   end
+    def namespaces
+      self.class.namespaces
+    end
+  end
 
-   def namespaces
-     name.split('::').slice(0...-1).map(&:underscore).map(&:to_s)
-   end
- end
+  private
+
+  def current_parent
+    parent if respond_to?(:parent, true)
+  end
+
+  def current_namespace
+    namespaces.first
+  end
+
+  def site_url
+    'site' if params[:site_id].present?
+  end
+
+  def site (current_resource = resource)
+    current_resource.site if current_resource.respond_to?(:site)
+  end
+
+  module ClassMethods
+    def current_namespace
+      namespaces.first
+    end
+
+    def namespaces
+      name.split('::').slice(0...-1).map(&:underscore).map(&:to_s)
+    end
+  end
 end

446f50edbd44eb89fcfb64bb562d325528c735d9
Joan <joan.martinez.ripoll@gmail.com>
2015-07-07 10:17:24 +0200

    cosmetic changes

--- lib/generators/foxinator/setup/templates/cms/app/controllers/concerns/routify.rb
+++ lib/generators/foxinator/setup/templates/cms/app/controllers/concerns/routify.rb
@@ -1,24 +1,24 @@
 module Routify
   extend ActiveSupport::Concern
 
-  included do
-    helper_method :current_namespace, :current_parent, :site_url, :site
+ included do
+   helper_method :current_namespace, :current_parent, :site_url, :site
+   
+   def namespaces
+     self.class.namespaces
+   end
+ end
 
-    def namespaces
-      self.class.namespaces
-    end
-  end
-
-  private
-
-  def current_parent
-    parent if respond_to?(:parent, true)
-  end
-
-  def current_namespace
-    namespaces.first
-  end
-
+ private
+ 
+ def current_parent
+   parent if respond_to?(:parent, true)
+ end
+ 
+ def current_namespace
+   namespaces.first
+ end
+  
   def site_url
     'site' if params[:site_id].present?
   end
@@ -27,13 +27,13 @@ module Routify
     current_resource.site if current_resource.respond_to?(:site)
   end
 
-  module ClassMethods
-    def current_namespace
-      namespaces.first
-    end
+ module ClassMethods
+   def current_namespace
+     namespaces.first
+   end
 
-    def namespaces
-      name.split('::').slice(0...-1).map(&:underscore).map(&:to_s)
-    end
-  end
+   def namespaces
+     name.split('::').slice(0...-1).map(&:underscore).map(&:to_s)
+   end
+ end
 end

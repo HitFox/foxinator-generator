c1a39e10814a9077706643ae28d310378c04cd16
Adam Bahlke <adam.bahlke@hitfoxgroup.com>
2015-07-16 15:54:11 +0200

    admins should not be able to register

--- lib/generators/foxinator/setup/templates/cms/app/models/admin.rb
+++ lib/generators/foxinator/setup/templates/cms/app/models/admin.rb
@@ -34,7 +34,7 @@ class Admin < ActiveRecord::Base
   #
   # 
   
-  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable, :recoverable, :rememberable, :trackable, :validatable
 
   #
   # Concerns
--- lib/generators/foxinator/setup/templates/roles_and_permissions/app/models/admin.rb
+++ lib/generators/foxinator/setup/templates/roles_and_permissions/app/models/admin.rb
@@ -37,7 +37,7 @@ class Admin < ActiveRecord::Base
   #
   # 
   
-  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable, :recoverable, :rememberable, :trackable, :validatable
 
   #
   # Concerns
